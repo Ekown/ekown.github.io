1eecd851e06016364f1ce3803649f2f42c1b9c45
Ekown <tancioco.eron@gmail.com>
2019-07-10 19:20:18 +0800

    Added animation and styles to tech stack progress bars (XS viewport only)
    - Animation for the progress bars
    - Color scaling for progress bar width
    - Legends for progress bar colors
    - Separated CSS files for each sections
    - Added scroll event listener for the start of animation

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,32 @@
 $(document).ready(function(){
+    // Determines the color of the progress bar by its width
+    function getProgressBarColor(aWidth) {
+        if (aWidth <= 33) 
+            return 'blue';
+        else if (aWidth > 33 && aWidth <= 66)
+            return 'green';
+        else if (aWidth > 66 && aWidth <= 100)
+            return 'red';
+        else
+            return 'gray';
+    }
+
+    // Animates the progress bars
+    function animateProgressBar(el) {
+        const progressBars = el.find('.progress-bar');
+
+        [...progressBars].forEach(progressBar => {
+            $(progressBar).animate({
+                width: $(progressBar).attr('aria-valuename'),
+            }, {
+                duration: 2000,
+                easing: 'linear',
+                step: function(x) {
+                    $(progressBar).css('background-color', getProgressBarColor(x))
+                }
+            });
+        });
+    }
 
     // Smooth scrolling function listener
     $(document).on('click', 'a.smooth-scroll', (e) => {
@@ -15,4 +43,16 @@ $(document).ready(function(){
             });
         }
     });
+
+    $(window).scroll(function() {
+        let hT = $('.tech-content').offset().top,
+            hH = $('.tech-content').outerHeight(),
+            wH = $(window).height(),
+            wS = $(this).scrollTop();
+            
+        if (wS > (hT - hH)) {
+            animateProgressBar($('.tech-content'));
+            $(window).off('scroll')
+        }
+     });
 });
