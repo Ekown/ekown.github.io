b535e2c9e63016e838a199ef13d60de07b9e0e3a
Eron Tancioco <etancioco@crmonline.com.au>
2022-03-15 00:06:55 +0800

    Move used font constants to a separate constant file and directory

---
+++ src/constants/used-fonts.js
@@ -0,0 +1,5 @@
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+
+export const USED_FONTS = [
+    faArrowRight,
+];
--- src/registerIcons.js
+++ src/registerIcons.js
@@ -1,14 +1,7 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
-import {
-    faArrowRight,
-    faB
-} from '@fortawesome/free-solid-svg-icons';
+import { faB } from '@fortawesome/free-solid-svg-icons';
+import { USED_FONTS } from './constants/used-fonts';
 
 export default function registerIcons() {
-    // @TODO Move the list of used icons to a separate constants file
-    const arUsedIcons = [
-        faArrowRight,
-    ];
-      
-    library.add(faB, ...arUsedIcons);
+    library.add(faB, ...USED_FONTS);
 }
