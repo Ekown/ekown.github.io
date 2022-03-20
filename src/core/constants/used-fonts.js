237334c43ff7592e40a74dbf42cdd46623170cb7
Ekown <Ekown@users.noreply.github.com>
2022-03-20 16:34:39 +0000

    deploy: db8b365c96721494db86baa4181a1c03730bfc46

--- src/core/constants/used-fonts.js
+++
@@ -1,5 +0,0 @@
-import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
-
-export const USED_FONTS = [
-    faArrowRight,
-];
--- src/registerIcons.js
+++
@@ -1,7 +0,0 @@
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faB } from '@fortawesome/free-solid-svg-icons';
-import { USED_FONTS } from './core/constants/used-fonts';
-
-export default function registerIcons() {
-    library.add(faB, ...USED_FONTS);
-}
