faac1fe1fe2472486d469d23bcd4f9229c06f847
Eron Tancioco <etancioco@crmonline.com.au>
2022-04-06 20:36:36 +0800

    Update Profile text

--- src/components/common/molecules/Profile/Profile.js
+++ src/components/common/molecules/Profile/Profile.js
@@ -12,9 +12,17 @@ const Profile = () => {
       <div className="row">
         <div className="text d-flex align-items-center h-center">
           <div>
-            <span>I was a Junior Software Engineer at </span>
-            <a href="https://www.honcho.com.au" target="_blank" rel="noreferrer">Honcho</a>
-            <span> in Pasay, Philippines. I have worked with different projects and technologies ranging from Zend to Angular. I handled both front-end and back-end tasks for the company. I have a thirst for knowledge and hunger for innovation. Let's make something new and exciting.</span>
+            <p>
+              Hi! My name is Eron and I'm a software engineer with a passion for improving old ideas and creating new exciting stuff.
+              I'm currently working at <a href="https://www.crmonline.com.au/">CRM Online Australia</a> building solutions for the SugarCRM platform and implementing features for mobile applications built using Ionic.
+            </p>
+            <p>
+              As a software engineer, I like bridging the gap between engineering and design, combining my technical expertise with my keen eye for aesthetics to produce a stunning product.
+              My long-term objective is to constantly design scalable and efficient apps that provide engaging, pixel-perfect user experiences while handling user data with elegance performance-wise.
+            </p>
+            <p>
+              When I'm not in front of a computer, I'm typically working out, watching Netflix, or crossing off another item on my bucket list.
+            </p>
           </div>
         </div>
       </div>
