ef97daa00eaf1b9929434b5fd11e5ab0e322640c
Eron Tancioco <etancioco@crmonline.com.au>
2022-05-09 19:51:43 +0800

    Add tools & framework logos

--- src/components/common/molecules/Experience/Experience.js
+++ src/components/common/molecules/Experience/Experience.js
@@ -13,6 +13,13 @@ import laravel from '../../../../assets/logo/laravel.png';
 import nodejs from '../../../../assets/logo/nodejs.png';
 import angular from '../../../../assets/logo/angular.png';
 import ionic from '../../../../assets/logo/ionic.png';
+import git from '../../../../assets/logo/git.png';
+import heroku from '../../../../assets/logo/heroku.png';
+import docker from '../../../../assets/logo/docker.png';
+import s3 from '../../../../assets/logo/s3.png';
+import codepipeline from '../../../../assets/logo/codepipeline.png';
+import cloudwatch from '../../../../assets/logo/cloudwatch.png';
+import cognito from '../../../../assets/logo/cognito.png';
 import { Image } from 'react-bootstrap';
 
 // TODO: Add a way to store the logos in an array/object for maintainability
@@ -70,6 +77,34 @@ const Experience = () => {
                 </div>
                 <div className="col-3 col-sm-2 col-md-2 d-flex">
                   <Image src={ionic} fluid={true} className="logo" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="row tools-platforms">
+            <div className="col-12">
+              <h3 className="row libraries-framework__label">Tools & Platforms</h3>
+              <div className="row libraries-framework__logos">
+                <div className="col-3 col-sm-2 col-md-2 d-flex">
+                  <Image src={git} fluid={true} className="logo" />
+                </div>
+                <div className="col-3 col-sm-2 col-md-2 d-flex">
+                  <Image src={heroku} fluid={true} className="logo" />
+                </div>
+                <div className="col-3 col-sm-2 col-md-2 d-flex">
+                  <Image src={docker} fluid={true} className="logo" />
+                </div>
+                <div className="col-3 col-sm-2 col-md-2 d-flex">
+                  <Image src={s3} fluid={true} className="logo" />
+                </div>
+                <div className="col-3 col-sm-2 col-md-2 d-flex">
+                  <Image src={codepipeline} fluid={true} className="logo" />
+                </div>
+                <div className="col-3 col-sm-2 col-md-2 d-flex">
+                  <Image src={cloudwatch} fluid={true} className="logo" />
+                </div>
+                <div className="col-3 col-sm-2 col-md-2 d-flex">
+                  <Image src={cognito} fluid={true} className="logo" />
                 </div>
               </div>
             </div>
