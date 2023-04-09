751e7d1455dabbfba3510b8dae74f02384a756f5
Eron Tancioco <etancioco@crmonline.com.au>
2023-04-09 16:07:09 +0800

    Fix react-intersection-observer behavior for the title components

--- src/components/sections/Skills/Skills.js
+++ src/components/sections/Skills/Skills.js
@@ -6,7 +6,7 @@ import Title from '../../common/atoms/Title/Title';
 
 const Skills = () => {
   const [ref, inView] = useInView({
-    threshold: 0.5,
+    threshold: 0.1,
     triggerOnce: true,
   });
 
