ae9aacd79cb97c7040e891d0dede8cb61b665ec2
Eron Tancioco <etancioco@crmonline.com.au>
2023-04-21 12:27:44 +0800

    Add resume section with responsiveness

---
+++ src/components/sections/Resume/Resume.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react';
+import { useInView } from 'react-intersection-observer';
+import style from './Resume.module.scss';
+
+const Resume = () => {
+  const [triggerTitle, setTriggerTitle] = useState(false);
+  const [ref, inView] = useInView({
+    threshold: 0.1,
+    triggerOnce: false,
+  });
+
+  useEffect(() => {
+    if (inView) {
+      document.title = 'Eron Tancioco | Resume';
+
+      setTriggerTitle(true);
+    }
+  }, [inView]);
+
+  return (
+    <section>
+      <div className={ style.resume } name="Resume" ref={ref}>
+        <div className="row">
+          <div className="col-12">
+            <h2 className="sub-text">Grab a copy of my Résumé</h2>
+            <div className="button">
+              <a className="ghost-button">Get a copy</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+Resume.propTypes = {};
+
+Resume.defaultProps = {};
+
+export default Resume;
