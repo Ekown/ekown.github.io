717b570ddba726fc3769b17e68757f7bea5a6f02
Eron Tancioco <etancioco@crmonline.com.au>
2023-04-25 19:31:28 +0800

    Add initial theme

--- craco.config.js
+++ craco.config.js
@@ -3,6 +3,7 @@ module.exports = {
         sass: {
             loaderOptions: {
                 additionalData: `
+                    @import "src/core/theme/theme";
                     @import "src/core/theme/mixins/responsive.scss";
                 `,
             },
