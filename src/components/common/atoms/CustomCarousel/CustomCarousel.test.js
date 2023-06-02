fc61bf6faf852dc52a857ad1017816d07069c53c
Eron Tancioco <etancioco@crmonline.com.au>
2023-06-03 04:55:20 +0800

    Add tests for the carousel controls

--- src/components/common/atoms/CustomCarousel/CustomCarousel.test.js
+++ src/components/common/atoms/CustomCarousel/CustomCarousel.test.js
@@ -23,3 +23,36 @@ test('should render properly', async () => {
 
   expect(lazyElement).toBeInTheDocument();
 });
+
+test('it should render carousel controls when the images length > 1', async () => {
+  const { getByTestId } = render(component({
+    images: ['image1.jpg', 'image2.jpg'],
+  }));
+
+  const customCarousel = await waitFor(() => getByTestId('custom-carousel'));
+
+  expect(customCarousel.getElementsByClassName('carousel-control-prev').length).toBe(1);
+  expect(customCarousel.getElementsByClassName('carousel-control-next').length).toBe(1);
+});
+
+test('it should not render carousel controls when the images length = 1', async () => {
+  const { getByTestId } = render(component({
+    images: ['image1.jpg'],
+  }));
+
+  const customCarousel = await waitFor(() => getByTestId('custom-carousel'));
+
+  expect(customCarousel.getElementsByClassName('carousel-control-prev').length).toBe(0);
+  expect(customCarousel.getElementsByClassName('carousel-control-next').length).toBe(0);
+});
+
+test('it should not render carousel controls when the images length < 1', async () => {
+  const { getByTestId } = render(component({
+    images: [],
+  }));
+
+  const customCarousel = await waitFor(() => getByTestId('custom-carousel'));
+
+  expect(customCarousel.getElementsByClassName('carousel-control-prev').length).toBe(0);
+  expect(customCarousel.getElementsByClassName('carousel-control-next').length).toBe(0);
+});
