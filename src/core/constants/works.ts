fb2f61cc854a7e0838aba5a164d14e4f50f603f6
Eron Tancioco <tancioco.eron@gmail.com>
2026-06-27 21:29:54 +0800

    update-arcanys-priceme: update WORKS and Work interface to include name and current properties for better project representation

--- src/core/constants/works.ts
+++ src/core/constants/works.ts
@@ -8,27 +8,37 @@ import { Work } from "../interfaces/works.interface";
 export const WORKS: Work[] = [
     {
         image: plm,
+        name: "Pamantasan ng Lungsod ng Maynila",
         link: "https://plm.edu.ph/",
         active: false,
+        current: false,
     },
     {
         image: bsw,
+        name: "Business Switch",
         link: "https://www.business-switch.com.au/",
         active: true,
+        current: false,
     },
     {
         image: crmonline,
+        name: "CRM Online",
         link: "https://www.crmonline.com.au/",
         active: true,
+        current: false,
     },
     {
         image: priceme,
+        name: "PriceMe",
         link: "https://priceme.game/",
         active: true,
+        current: false,
     },
     {
         image: arcanys,
+        name: "Arcanys",
         link: "https://arcanys.com/",
         active: true,
+        current: true,
     },
 ];
--- src/core/interfaces/works.interface.ts
+++ src/core/interfaces/works.interface.ts
@@ -1,5 +1,7 @@
 export interface Work {
     image: string;
+    name: string;
     link: string;
     active: boolean;
+    current: boolean;
 }
